58dd37c3f14943483aa07508ea0f812762bb8ea1
Sivasubramani S <siva.subramani975@gmail.com>
2021-04-11 01:19:31 +0530

    Change of directory structure

---
+++ Bash/check_gtf.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+gtf=$1
+feature_type=$2
+
+function check_feature_type_present(){
+    ft=$1
+    local first_feature=$(awk '{print $3}' $gtf | grep -m 1 $ft)
+    feature_status=$?
+    return $feature_status
+}
+
+function count_features(){
+    ft=$1
+    
+    n_features=$(awk -v type=$ft '{ if ($3 == type) { print } }' $gtf | wc -l | sed 's/ //g')
+    if [ "$n_features" -eq '0' ]; then
+        echo "Something wrong, can't find any $ft features" 1>&2
+        return 1
+    else
+        echo $n_features
+    fi
+}
+
+# Check if the feature type is present before proceeding
+
+ft_present=$(check_feature_type_present $feature_type)
+if [ $? -eq 0 ]; then
+    echo "Feature type $feature_type is present"
+else
+    echo "Feature type $feature_type is not present, choose another"
+    exit 1
+fi
+
+# Count the features
+nf=$(count_features $feature_type)
+if [ "$?" -eq '0' ]; then
+    echo "Got a count of $nf"
+else
+    echo "Failed to get a count for $feature_type features in GTF content"
+fi
