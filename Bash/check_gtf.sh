a574505386416f96f08d1baf6dc223adef0b7dad
Sivasubramani S <siva.subramani975@gmail.com>
2021-04-11 01:33:35 +0530

    Modified   Bash/check_gtf.sh

--- Bash/check_gtf.sh
+++ Bash/check_gtf.sh
@@ -9,7 +9,12 @@ function check_feature_type_present(){
     ft=$1
     local first_feature=$(awk '{print $3}' $gtf | grep -m 1 $ft)
     feature_status=$?
-    return $feature_status
+    if [ "$first_feature" == '' ]; then
+        echo "Something wrong, can't find any $ft features" 1>&2
+        exit 1
+    else
+        return $feature_status
+    fi
 }
 
 function count_features(){
@@ -27,6 +32,7 @@ function count_features(){
 # Check if the feature type is present before proceeding
 
 ft_present=$(check_feature_type_present $feature_type)
+echo $ft_present
 if [ $? -eq 0 ]; then
     echo "Feature type $feature_type is present"
 else
